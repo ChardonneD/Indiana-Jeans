e69e672a7b7f9dce00add3326b3e3eb3a3b5d5bd
ChardonneDixon <chardonne.dixon@gmail.com>
2026-05-02 13:38:41 -0500

    cleaned up syntax errors.

--- scripts/LocationChoices.js
+++ scripts/LocationChoices.js
@@ -4,17 +4,18 @@ export const LocationChoices = async () => {
     const response = await fetch("http://localhost:8088/socioLocations")
     const locations = await response.json()
 
+    // Build HTML for radio buttons for location choices
     let html = `
         <div class="survey-input" id="location-choice">
             <h2>What type of area do you live in?</h2>
     `
-    
+ // Loop through locations and create a radio button for each   
 for (const location of locations) {
-        html += ` <input type="radio" name="location" value="${location.id}" /> ${location.label}`
+        html += `
+        <input type="radio" name="location" value="${location.id}" />
+        ${location.label}`
         }
-    html += `
-        </div>
-    `
+    html += `</div>    `
 
     return html
 }
--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ import { LocationChoices } from "./LocationChoices.js"
 const container = document.querySelector("#container")
 
 const render = async () => {
-    const jeansHTML = JeanChoices()
+    const jeansHTML = await JeanChoices()
     const locationHTML = await LocationChoices()
     
     container.innerHTML = `
